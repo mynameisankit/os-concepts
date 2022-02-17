ca9513189cfc1a4e0dcee430f5b126a9fdb1c312
Ankit Kumar <kumar.ankit.862001@gmail.com>
2022-02-18 00:12:42 +0530

    Initial Commit

---
+++ process-concepts/zombie_process.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+//To run the program for 10 seconds
+#include<signal.h>
+
+int main() {
+    /**
+     * What is a zombie process?
+     * It is a child process of some parent process, which has completed 
+     * its execution and whose parent has not yet called wait() system call 
+     * to obtain the status or has terminated and not yet been assigned 
+     * to init process as parent and still has its entry in the process table
+     * */
+
+    pid_t pid = fork();
+
+    //Child Process
+    if(pid == 0) {
+        //Run the child process for atleast 10 seconds
+        exit(0);
+    }
+    //Parent Process
+    else {
+        //Do not call the wait() system call
+        //wait()
+
+        //Print the child process ID
+        printf("Child Process : %d\n", pid);
+
+        //Run parent for 10 seconds
+        sleep(10);
+    }
+
+    return 0;
+}
